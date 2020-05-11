e4948b76371a80db78c03286c56377b9df25c3cc
satyamrastogi <satyam.mnnit97@gmail.com>
2020-05-12 01:33:20 +0530

    Updates

---
+++ precache-manifest.64c9b365f7c019e3951b38f78a91956a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12acf03baec015b476e7174ddadd727c",
+    "url": "/reactTodo/index.html"
+  },
+  {
+    "revision": "3f87441b7879eb116ab7",
+    "url": "/reactTodo/static/css/2.c3343ac3.chunk.css"
+  },
+  {
+    "revision": "232d4280e292818f8d9b",
+    "url": "/reactTodo/static/css/main.28edbf94.chunk.css"
+  },
+  {
+    "revision": "3f87441b7879eb116ab7",
+    "url": "/reactTodo/static/js/2.c6400c98.chunk.js"
+  },
+  {
+    "revision": "232d4280e292818f8d9b",
+    "url": "/reactTodo/static/js/main.eee0b4c8.chunk.js"
+  },
+  {
+    "revision": "49bb0973648c6317334b",
+    "url": "/reactTodo/static/js/runtime-main.4c18db7c.js"
+  }
+]);
